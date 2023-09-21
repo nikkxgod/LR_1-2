a4349252d23eca3cd4f82fe3a7987d6a2eed64be
nikkxgod <100446613+nikkxgod@users.noreply.github.com>
2023-09-21 12:48:02 +0300

    Update блок 3 задача 1.pas

--- блок 3 задача 1.pas
+++ блок 3 задача 1.pas
@@ -1,7 +1,11 @@
-﻿var x:integer;
-  begin
-    writeln('Введите год');
+var x:integer;
+begin
+    writeln('Введите год')
     read(x);
-    if (x mod 4) = 0 then write(x,' год високосный')
-    else write(x,' год невисокосный');
-  end.
+    if x mod 4 <> 0 then writeln('Невисокосный')
+    else
+        if x mod 100 = 0 then
+            if x mod 400 = 0 then writeln('Високосный')
+            else writeln('Невисокосный')
+        else writeln('Високосный');
+end.
